fa489663a14f681d524e287638f4fa72433b01cd
Vannent <erobced@gmail.com>
2022-04-14 14:44:34 -0500

    feat: add partial content to resume container

--- src/components/Resume.js
+++ src/components/Resume.js
@@ -24,6 +24,49 @@ const Resume = () => {
             </div>
           </div>
         </div>
+        <div className="resume-about">
+          <div className="about-title">Summary</div>
+          <div className="about-content">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
+            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+            aliquip ex ea commodo consequat.
+          </div>
+        </div>
+        <div className="resume-skills">
+          <div className="skills-title">Skills</div>
+          <div className="skills-list">
+            <li className="skills">Skill</li>
+            <li className="skills">Skill</li>
+            <li className="skills">Skill</li>
+          </div>
+        </div>
+        <div className="resume-projects">
+          <div className="project-title">Projects</div>
+          <div className="projects-list">
+            <li className="projects">Project</li>
+            <p>
+              Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit
+              aut fugit
+            </p>
+            <li className="projects">Project</li>
+            <p>
+              Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit
+              aut fugit
+            </p>
+            <li className="projects">Project</li>
+            <p>
+              Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit
+              aut fugit
+            </p>
+          </div>
+        </div>
+        <div className="resume-experience">
+          <div className="experience-title">Experience</div>
+        </div>
+        <div className="resume-education">
+          <div className="education-title">Education</div>
+        </div>
       </div>
     </div>
   );
