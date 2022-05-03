e655e01bf218408733abed7f683cba9f8e4d4f58
Vannent <erobced@gmail.com>
2022-05-03 12:30:16 -0500

    feat: add resume and handleHome props

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,18 +1,39 @@
 import Header from "../components/Header";
 import NavBar from "../components/NavBar";
 import Resume from "../components/Resume";
+import Edit from "../components/Edit";
 import "../styles/Home.css";
+import { useState } from "react";
 
-const Home = () => {
+const Home = (props) => {
+  const [resume, setResume] = useState({
+    name: "First Last",
+    role: "Your Role",
+    phone: "000-000-000",
+    email: "email@mail.com",
+    linkedin: "linkedin.com/in/username/",
+    summary: "example example example",
+    skills: "skills, skills, skills",
+    projects: "project, project, project",
+    experience: "experience, experience, experience",
+    education: "education, education, education",
+  });
+
+  const [handleHome, setHandleHome] = useState(false);
+  const handleClick = () => {
+    if (handleHome === false) setHandleHome(true);
+    else setHandleHome(false);
+  };
+
   return (
     <div className="home">
       <Header />
       <div className="container">
         <div className="navbar">
-          <NavBar />
+          <NavBar data={handleClick} />
         </div>
         <div className="content">
-          <Resume />
+          {handleHome ? <Edit resume={resume} /> : <Resume resume={resume} />}
         </div>
       </div>
     </div>
