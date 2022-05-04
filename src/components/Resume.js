60439ad7c14ce157f92282d5f12caa55529f43a1
Vannent <erobced@gmail.com>
2022-05-03 19:58:52 -0500

    fix: remove unused code and fix props

--- src/components/Resume.js
+++ src/components/Resume.js
@@ -1,78 +1,60 @@
 import React from "react";
 
-const Resume = () => {
+const Resume = (props) => {
   return (
     <div className="resume-container">
       <div className="resume">
         <div className="resume-header">
-          <div className="resume-name">First Last</div>
-          <div className="resume-role">Your Role</div>
+          <div className="resume-name">{props.resume.name}</div>
+          <div className="resume-role">{props.resume.role}</div>
           <div className="resume-contact">
             <div className="resume-contact-box">
               <div className="resume-ask">Phone:</div>
-              <div className="resume-phone-input">000-000-000</div>
+              <div className="resume-phone-input">{props.resume.phone}</div>
             </div>
             <div className="resume-contact-box">
               <div className="resume-ask">Email:</div>
-              <div className="resume-phone-input">youremail@mail.com</div>
+              <div className="resume-phone-input">{props.resume.email}</div>
             </div>
             <div className="resume-contact-box">
               <div className="resume-ask">LinkedIn:</div>
-              <div className="resume-phone-input">
-                linkedin.com/in/lauren-chen/
-              </div>
+              <div className="resume-phone-input">{props.resume.linkedin}</div>
             </div>
           </div>
         </div>
         <div className="resume-about">
           <div className="about-title">Summary</div>
-          <div className="about-content">
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
-            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-            aliquip ex ea commodo consequat.
-          </div>
+          <div className="about-content">{props.resume.summary}</div>
         </div>
         <div className="resume-skills">
           <div className="skills-title">Skills</div>
           <div className="skills-list">
-            <div className="skills">Skill</div>
-            <div className="skills">Skill</div>
-            <div className="skills">Skill</div>
+            <div className="skills">{props.resume.skills}</div>
           </div>
         </div>
         <div className="resume-projects">
           <div className="project-title">Projects</div>
           <div className="projects-list">
-            <div className="projects">Project</div>
-            <p>
-              Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit
-              aut fugit
-            </p>
-            <div className="projects">Project</div>
-            <p>
-              Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit
-              aut fugit
-            </p>
-            <ddiv className="projects">Project</ddiv>
-            <p>
-              Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit
-              aut fugit
-            </p>
+            <div className="projects">{props.resume.projectTitle}</div>
+            <p>{props.resume.project}</p>
           </div>
         </div>
         <div className="resume-experience">
           <div className="experience-title">Experience</div>
-          <h3 className="resume-experience-title">Software Engineer</h3>
+          <h3 className="resume-experience-title">
+            {props.resume.experienceTitle}
+          </h3>
           <p className="resume-experience-description">
-            Manage and design applications with JavaScript and HTML.
+            {props.resume.experience}
           </p>
         </div>
         <div className="resume-education">
           <div className="education-title">Education</div>
-          <h3 className="resume-education-title">The Odin Project</h3>
+          <h3 className="resume-education-title">
+            {props.resume.educationTitle}
+          </h3>
           <p className="resume-education-description">
-            Manage and design applications with JavaScript and HTML.
+            {props.resume.education}{" "}
           </p>
         </div>
       </div>
