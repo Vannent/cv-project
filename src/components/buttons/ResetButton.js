2e09175205421f80bd66113826e09843224dbfdb
Vannent <erobced@gmail.com>
2022-05-03 20:26:08 -0500

    feat: add functionality to reset

--- src/components/buttons/ResetButton.js
+++ src/components/buttons/ResetButton.js
@@ -2,8 +2,11 @@ import React from "react";
 import resetIcon from "../../img/reset.svg";
 
 const ResetButton = () => {
+  const refreshPage = () => {
+    window.location.reload();
+  };
   return (
-    <li>
+    <li onClick={refreshPage}>
       <img src={resetIcon} alt="reset icon" />
       Reset
     </li>
